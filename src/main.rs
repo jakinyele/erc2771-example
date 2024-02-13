2f6577d482d8ad67c2e4d60c3c8c02a941b26486
J. Ayo Akinyele <ayo@boltlabs.io>
2024-02-13 00:51:52 -0500

    more clean up

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 use alloy_primitives::Address;
 use alloy_sol_types::{Eip712Domain, eip712_domain, SolStruct};
 use alloy_primitives::{U256, keccak256};
-use alloy_sol_types::sol;
-use std::str::FromStr;
+use alloy_sol_types::{sol, SolCall};
+use alloy_primitives::hex_literal::hex;
 
 sol! {
      struct ForwardRequest {
@@ -15,6 +15,11 @@ sol! {
         uint48 deadline;
         bytes data;
      }
+
+     #[derive(Debug, PartialEq)]
+     interface IERC20 {
+         function transfer(address to, uint256 amount) external returns (bool);
+     }
 }
 
 fn main() {
@@ -23,24 +28,31 @@ fn main() {
         name: "Meta Transaction Forwarder",
         version: "1",
         chain_id: 1115111,
-        verifying_contract: Address::from_str("0xBE4dcaE032a02110e37c4767B8EC6bFd40bdF5b7").unwrap(),
+        verifying_contract: Address::from(hex!("BE4dcaE032a02110e37c4767B8EC6bFd40bdF5b7")),
         // salt: keccak256("my domain salt"),
     };
     
     // vec for 0xa9059cbb000000000000000000000000966ef13ff6974ec7b7018cdbdedd77e9bffc33670000000000000000000000000000000000000000000000000000000000000001
-    let data_vec = vec![169, 5, 156, 187, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 150, 110, 241, 63, 246, 151, 78, 199, 183, 1, 140, 219, 222, 221, 119, 233, 191, 252, 51, 103, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
-    let hashed_data_vec = keccak256(data_vec.clone());
+    let data = hex!(
+        "a9059cbb"
+        "000000000000000000000000966ef13ff6974ec7b7018cdbdedd77e9bffc3367"
+        "0000000000000000000000000000000000000000000000000000000000000001"
+    );
+    assert_eq!(data[..4], IERC20::transferCall::SELECTOR);
+    // assert_eq!(decoded.encode(), data);
+
+    let hashed_data_vec = keccak256(data.clone());
 
     println!("Hashed Data: {}", hashed_data_vec);
 
     let forward_request_message = ForwardRequest {
-        from: "0x1d98BF1FE5ae430A98461bAd3b872031767c9634".parse().unwrap(),
-        to: "0x59f913ae23172a22Ffc2846a56c0E8d7636B927F".parse().unwrap(),
+        from: Address::from(hex!("1d98BF1FE5ae430A98461bAd3b872031767c9634")),
+        to: Address::from(hex!("59f913ae23172a22Ffc2846a56c0E8d7636B927F")),
         value: U256::from(0),
         gas: U256::from(100000),
         nonce: U256::from(0),
         deadline: 1707594318 as u64,
-        data: data_vec.to_vec(),
+        data: data.to_vec(),
     }; 
 
     let encoded_type = ForwardRequest::eip712_encode_type();
